03014ece28576b97ca05188696cbf1e91e6a77ab
seyi4mi <seyi4mi99@gmail.com>
2021-10-13 21:45:55 +0800

    made contact us responsive

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Aboutus from "./components/About/Aboutus"
 import VidSection from "./components/Video/VidSection"
 // import Services from "./components/Services/Services"
 // import OurTeam from "./components/Team/OurTeam"
-// import Contact from "./components/Contact/Contact"
+import Contact from "./components/Contact/Contact"
 import Footer from "./components/Footer/Footer"
 
 function App() {
@@ -15,8 +15,8 @@ function App() {
 			<Navbar />
 			<Banner />
 			<Aboutus />
-			<VidSection /> {/* <Services /> */} {/* <OurTeam /> */}{" "}
-			{/* <Contact /> */} <Footer />
+			<VidSection /> {/* <Services /> */} {/* <OurTeam /> */} <Contact />
+			<Footer />
 		</>
 	)
 }
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,23 +7,25 @@ function Contact() {
 			<div className="contact" id="contact-us">
 				<div className="contacts">
 					<div className="contact-info">
-						<h2 className="contact-title"> Contact Us </h2>{" "}
+						<h2 className="contact-title"> Contact Us </h2>
 						<div>
-							<i className="fa fa-envelope"> </i>email@gmail.com{" "}
-						</div>{" "}
+							<i className="fa fa-envelope"> </i>email@gmail.com
+						</div>
 						<div>
-							<i className="fa fa-phone"> </i>+60 123 456 789{" "}
-						</div>{" "}
+							<i className="fa fa-phone"> </i>+60 123 456 789
+						</div>
 						<div>
-							<i className="fa fa-map-marker"> </i>address, Malaysia, Kuala
-							Lumpur{" "}
-						</div>{" "}
+							<div className="marker-icon">
+								<i className="fa fa-map-marker"> </i>address, Malaysia, Kuala
+								Lumpur
+							</div>
+						</div>
 					</div>
+
 					<div className="contact-form">
 						<h2 h2 className="intouch-title">
-							{" "}
-							Let 's Get In Touch
-						</h2>{" "}
+							Let's Get In Touch
+						</h2>
 						<form action="" method="post">
 							<input
 								type="text"
@@ -31,30 +33,30 @@ function Contact() {
 								className="text-box"
 								placeholder=" &#xf007;  Your Name"
 								required
-							></input>{" "}
+							></input>
 							<input
 								type="email"
 								name="email"
 								className="text-box"
 								placeholder=" &#xf0e0;  Your Email"
 								required
-							></input>{" "}
+							></input>
 							<textarea
 								name="message"
 								className="msg-box"
 								rows="5"
 								placeholder="Your Message"
-							></textarea>{" "}
+							></textarea>
 							<input
 								type="submit"
 								name="submit"
 								className="submit-btn"
 								value="Submit"
-							></input>{" "}
-						</form>{" "}
-					</div>{" "}
-				</div>{" "}
-			</div>{" "}
+							></input>
+						</form>
+					</div>
+				</div>
+			</div>
 		</>
 	)
 }
